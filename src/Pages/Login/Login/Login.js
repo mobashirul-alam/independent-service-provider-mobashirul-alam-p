a7101ccddc013c24c8b9ac5c32ba9d87900532ad
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-04-18 14:48:54 +0600

    register form designed and login modified

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div>
-            <div className='w-50 shadow mx-auto border border-primary rounded-3 my-5'>
+            <div className='w-50 shadow mx-auto border border-primary rounded-3 my-3'>
                 <h1 className='text-center my-3'>Please Login</h1>
                 <Form className='w-75 mx-auto my-3'>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -15,13 +16,20 @@ const Login = () => {
                         <Form.Label>Password</Form.Label>
                         <Form.Control type="password" placeholder="Password" />
                     </Form.Group>
-                    {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group> */}
                     <Button variant="primary" type="submit">
                         Login
                     </Button>
                 </Form>
+                <div className='w-75 mx-auto'>
+                    <p>
+                        New to Pro-Fit ?
+                        <Link className='text-decoration-none' to="/register"> Please Register</Link>
+                    </p>
+                    <p>
+                        Forget Password ?
+                        <Link className='text-decoration-none' to="/register"> Reset Password</Link>
+                    </p>
+                </div>
             </div>
         </div>
     );
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
         <div>
-            <h1>This is register page</h1>
+            <div className='w-50 shadow mx-auto border border-primary rounded-3 my-3'>
+                <h1 className='text-center my-3'>Please Register</h1>
+                <Form className='w-75 mx-auto my-3'>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Your name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter your name" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Register
+                    </Button>
+                </Form>
+                <p className='w-75 mx-auto'>
+                    Already have an account ?
+                    <Link className='text-decoration-none' to="/login"> Please Login</Link>
+                </p>
+            </div>
         </div>
     );
 };
